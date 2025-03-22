1ee80e577295bee6b226fa4f7a48661394b1bbe8
damjanPetr <pedamovski@gmail.com>
2025-03-22 11:09:45 +0100

    Add SQLite database connection and update dependencies
    add db from other project  update TypeScript and Vite to newer versions

---
+++ src/utils/db.ts
@@ -0,0 +1,15 @@
+import { Sequlize } from "sequlize";
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'C:\\Users\\praska\\Desktop\\tools\\benign-key-logger-master\\dist\\key_logger\\key_log.sqlite'
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+export { sequelize }
