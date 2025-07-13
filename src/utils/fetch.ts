6cb2f1d0d09623f2420cb47ad345f9fabf017c0a
damjanPetr <pedamovski@gmail.com>
2025-07-13 18:50:37 +0200

    fix: fix vite base property in config

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,13 +2,13 @@ import { parse } from "yaml";
 import type { KeysideData, LayoutData } from "../types";
 
 export async function changeLoadout(layout: string): Promise<KeysideData> {
-	const response = await fetch(`/backend/${layout}.yaml`);
+	const response = await fetch(`backend/${layout}.yaml`);
 	const data = await response.text();
 	return parse(data);
 }
 
 export async function fetchKeyLayout(side: string): Promise<LayoutData> {
-	const response = await fetch(`/backend/layouts/${side}.yaml`);
+	const response = await fetch(`backend/layouts/${side}.yaml`);
 	const data = await response.text();
 	return parse(data);
 }
